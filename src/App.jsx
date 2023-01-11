b464877f43d13d568feedd231c1f271fe9f96156
Strifexss <rocombole0007@gmail.com>
2023-01-11 16:41:06 -0300

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
+import Cabecalho from "./Cabecalho";
 import Fade from "react-reveal/Fade"
 function App() {
   return (
     <div>
-      <Fade>
-      <h1>
-        Teste
-      </h1>
-      </Fade>
+      <Cabecalho/>
     </div>
   );
 }
---
+++ src/Cabecalho.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+const Main = styled.header`
+    color: red;
+`
+
+export default function Cabecalho() {
+    return(
+        <header>
+            <h1>Teste</h1>
+        </header>
+    )
+}
